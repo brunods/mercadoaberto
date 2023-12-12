8320df4eb17ea9efc01da1b49add45842e67d96a
Igor Barreto <igor.cientista@gmail.com>
2023-12-12 20:47:28 +0000

    add: image type enum

---
+++ app/Enums/ImageTypeEnum.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+enum ImageTypeEnum: string
+{
+    case AVATAR = 'AVATAR';
+    case ENTERPRISE = 'ENTERPRISE';
+    case LOGO = 'LOGO';
+    case PRODUCTS = 'PRODUCTS';
+}
