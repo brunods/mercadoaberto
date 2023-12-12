3c3e3034beab5c72fabc359a2857ec5ff6f99593
Igor Barreto <igor.cientista@gmail.com>
2023-12-12 20:49:02 +0000

    feat: add link to upload image

--- app/Traits/ObjectStorageTrait.php
+++ app/Traits/ObjectStorageTrait.php
@@ -6,6 +6,20 @@ use Illuminate\Support\Facades\Storage;
 
 trait ObjectStorageTrait
 {
+    public function getLinkToUploadImage($imageType, $imageExtension, $userId, $bucket)
+    {
+        $disk = Storage::disk($bucket);
+        $filename = strtolower($imageType).'/'.$userId.'_'.time().'.'.$imageExtension;
+
+        $disk->put($filename, '');
+        $link = $disk->temporaryUrl($filename, now()->addMinutes(5));
+
+        return [
+            'filename' => $filename,
+            'link' => $link,
+        ];
+    }
+
     public function deleteFile($filename, $bucket)
     {
         $disk = Storage::disk($bucket);
