5bb09454a4649f6661c37feb277f0687e2368408
Igor Barreto <igor.cientista@gmail.com>
2023-12-13 22:11:22 +0000

    chore: refactor to use aws sdk

--- app/Traits/ObjectStorageTrait.php
+++ app/Traits/ObjectStorageTrait.php
@@ -2,30 +2,61 @@
 
 namespace App\Traits;
 
+use Aws\Credentials\Credentials;
+use Aws\S3\S3Client;
 use Illuminate\Support\Facades\Storage;
 
 trait ObjectStorageTrait
 {
-    public function getLinkToUploadImage($imageType, $imageExtension, $userId, $bucket)
-    {
-        $disk = Storage::disk($bucket);
-        $filename = strtolower($imageType).'/'.$userId.'_'.time().'.'.$imageExtension;
+    private $s3;
 
-        $disk->put($filename, '');
-        $link = $disk->temporaryUrl($filename, now()->addMinutes(5));
+    public function __construct()
+    {
+        $access_key_id = env('R2_ACCESS_KEY');
+        $access_key_secret = env('R2_SECRET_KEY');
+        $endpoint = env('R2_ENDPOINT');
+        $credentials = new Credentials($access_key_id, $access_key_secret);
+
+        $options = [
+            'region' => 'us-east-1',
+            'endpoint' => $endpoint,
+            'version' => 'latest',
+            'credentials' => $credentials,
+        ];
+
+        $this->s3 = new S3Client($options);
+    }
+
+    public function getLinkToUploadImage($imageType, $imageExtension, $folder, $userId, $bucket)
+    {
+        $filename = strtolower($folder).'/'.$userId.'_'.time().'.'.$imageExtension;
+
+        $presignedUrl = $this->createPresignedUrl($filename, $bucket, 'PutObject');
 
         return [
-            'filename' => $filename,
-            'link' => $link,
+            'url' => $presignedUrl,
         ];
     }
 
     public function getImage($filename, $bucket)
     {
-        $disk = Storage::disk($bucket);
-        $link = $disk->temporaryUrl($filename, now()->addMinutes(5));
+        $presignedUrl = $this->createPresignedUrl($filename, $bucket, 'GetObject');
 
-        return $link;
+        return [
+            'url' => $presignedUrl,
+        ];
+    }
+
+    public function createPresignedUrl($filename, $bucket, $operation)
+    {
+        $cmd = $this->s3->getCommand($operation, [
+            'Bucket' => $bucket,
+            'Key' => $filename,
+        ]);
+
+        $request = $this->s3->createPresignedRequest($cmd, '+1 hour');
+
+        return (string) $request->getUri();
     }
 
     public function deleteFile($filename, $bucket)
