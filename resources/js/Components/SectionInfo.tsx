2d00579e851c53b39388e8969ec0c7fbc77d7b1e
Bruno Duarte <bruno.c1.2@hotmail.com>
2023-12-20 23:34:56 -0300

    FrontEnd da tela de login #191

---
+++ resources/js/Components/SectionInfo.tsx
@@ -0,0 +1,11 @@
+export default function SectionInfo() {
+    return (
+        <section className="w-full hidden flex-col justify-center items-center bg-secondary lg:flex">
+            <div className="p-[32px] sm:w-[448px]">
+                <div className="w-fit">
+
+                </div>
+            </div>
+        </section>
+    );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+import { Primary } from '@storybook/blocks';
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -15,7 +16,19 @@ export default {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                inter: ['Inter', ...defaultTheme.fontFamily.sans],
+                epilogue: ['Epilogue', ...defaultTheme.fontFamily.sans],
             },
+            colors:{
+                primary: '#516AD2',
+                secondary: '#0041A1',
+                gray: {
+                    100: '#DDD',
+                    200: '#C9C6C6',
+                    600: '#767171',
+                    900: '#3B3838',
+                },
+            }
         },
     },
 
